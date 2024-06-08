3d6d7cbe766c5eb22904d3428648b278954ecc71
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-06-07 20:09:10 -0400

    added ai api integration and set up response driven button

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
-function poemGenerator(event){
-event.preventDefault();
-
-
+function displayPoem(response){
 new Typewriter("#poem", {
-  strings: "sample of what a poem will look like",
+  strings: response.data.answer,
   autoStart: true,
   cursor: "",
   delay: 1,
 });
+}
+
+
+function poemGenerator(event){
+event.preventDefault();
+
+let instructions = document.querySelector("#user-instructions");
+let apiKey = "01dd2bca25c0t00b3d253f443e0of791";
+let context = "you are a creative and romantic poem writer. Your objective is to create a 4 line poem, in HTML format seperating each line with a <br>, following the user instructions. only display the poem.";
+let prompt = `user instructions are: generate a poem about ${instructions.value}` ;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("Generating a poem");
+console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`);
+
+axios.get(apiUrl).then(displayPoem);
 }
 
 let formInputElement = document.querySelector("#form-input");
